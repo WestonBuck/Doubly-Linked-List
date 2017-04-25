141790b7ec944a75cc86df4d318141428dc85163
WestonBuck <weezygc@gmail.com>
2017-04-25 16:16:07 -0500

    Initial Uplaod
    Has linkedlist class completed.

---
+++ src/com/company/Main.java
@@ -0,0 +1,136 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        
+    }
+
+    /**
+     * Created by Weezy on 4/25/2017.
+     */
+
+    public static class DoublyLinkedList<E>
+    {
+
+        private static class Node<E>
+        {
+            private E element;
+            private Node<E> prev;
+            private Node<E> next;
+            public Node (E e, Node<E> p , Node<E> n)
+            {
+                element = e;
+                prev = p;
+                next = n;
+            }
+
+            public E getElement()
+            {
+                return element;
+            }
+
+            public Node<E> getPrev()
+            {
+                return prev;
+            }
+            public Node<E> getNext()
+            {
+                return next;
+            }
+            public void setPrev(Node<E> p)
+            {
+                prev = p;
+            }
+            public void setNext(Node<E> n)
+            {
+                next = n;
+            }
+        }
+
+        private Node<E> header;
+        private Node<E> trailer;
+        private int size = 0;
+
+        public DoublyLinkedList()
+        {
+            header = new Node<>(null,null,null);
+            trailer = new Node<>(null,null,null);
+            header.setNext(trailer);
+        }
+
+
+        public int size()
+        {
+            return size;
+        }
+
+        public boolean isEmpty ()
+        {
+            return size == 0;
+        }
+
+        public E first ()
+        {
+            if (isEmpty())
+            {
+                return null;
+            }
+            return  header.getNext().getElement();
+        }
+
+        public E last ()
+        {
+            if (isEmpty())
+            {
+                return null;
+            }
+            return  header.getPrev().getElement();
+        }
+
+        public void addFirst (E e)
+        {
+            addBetween(e,header,header.getNext());
+        }
+
+        public void addLast(E e)
+        {
+            addBetween(e,trailer.getPrev(),trailer);
+        }
+
+        public E removeFirst()
+        {
+            if (isEmpty()) {
+                return null;
+            }
+            return remove(header.getNext());
+        }
+
+        public E removeLast()
+        {
+            if (isEmpty()) {
+                return null;
+            }
+            return remove(trailer.getPrev());
+        }
+
+        private void addBetween(E e, Node predecessor, Node successor)
+        {
+             Node<E> newest = new Node<>(e,predecessor,successor);
+             predecessor.setNext(newest);
+             successor.setPrev(predecessor);
+             size++;
+        }
+
+        private E remove (Node<E> node)
+        {
+            Node<E> predessor = node.getPrev();
+            Node<E> succcessor = node.getNext();
+            predessor.setNext(succcessor);
+            succcessor.setPrev(predessor);
+            size--;
+            return node.getElement();
+        }
+    }
+}
